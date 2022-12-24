49b74901ed573098ce130c3f46ce591575324f19
Goodkatt <gorkem444altunay@hotmail.com>
2022-12-24 15:18:49 +0300

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import React, { Component } from 'react';
+import axios from 'axios';
+
+
+class App extends Component {
+  state = {
+    selectedFile: null,
+    fileUploadedSuccessfully: false
+  }
+
+  onFileChange = event => {
+    this.setState({selectedFile: event.target.files[0]});
+  }
+  onFileUpload = () => {
+    const formData = new FormData();
+    formData.append(
+      "demo file",
+      this.state.selectedFile,
+      this.state.selectedFile.name
+    )
+    //call api
+  
+    axios.post("https://61pks9svcj.execute-api.us-east-1.amazonaws.com/v1/upload", formData, {
+      headers: {
+        'Content-Type': 'image/jpeg'
+      }
+    }).then(() => {
+      this.selectedFile({selectedFile: null});
+      this.setState({fileUploadedSuccessfully: true});
+    })
+    .catch(error => console.log(error));
+//    axios.post("https://66pyrc1adf.execute-api.us-east-1.amazonaws.com/prod/grkm-test-bucket/asd.jpeg")
+    console.log(formData);
+
+  }
+
+  fileData = () => {
+    if (this.state.selectedFile) {
+      return(
+      <div>
+        <h2>File details</h2>
+        <p>File Name: {this.state.selectedFile.name}</p>
+        <p>File Type: {this.state.selectedFile.type}</p>
+      </div>
+      )
+    }
+    else if (this.state.fileUploadedSuccessfully) {
+      return (
+        <div>
+          <br />
+          <h4>Fike uploaded successfully</h4>
+        </div>
+      )
+    }
+    else {
+      return(
+        <div>
+          <br />
+          <h4>Choose a file</h4>
+        </div>
+      )
+    }
+  }
+
+
+  render() {
+    return(
+      <div className="container">
+        <h2>File upload system</h2>
+        <h3>file upload serverless</h3>
+        <div>
+          <input type="file" onChange={this.onFileChange} />
+          <button onClick={this.onFileUpload}>Upload</button>
+        </div>
+        {this.fileData()}
+      </div>
+    )
+  }
+
 }
 
 export default App;
